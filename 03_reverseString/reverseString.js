80cd0de23883df967b362cf1091d539c8be09f2d
MBanerjea <mayukh.flip22@gmail.com>
2023-01-27 17:39:33 +0530

    completes 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const strLength = string.length;
+  let reversedString = "";
+  for (let i = strLength - 1; i >= 0; i--) {
+    reversedString += string.charAt(i);
+  }
+  return reversedString;
 };
 
 // Do not edit below this line
