12bf9d4cdcbaba9375f235e213e25dfef66e5215
MBanerjea <mayukh.flip22@gmail.com>
2023-01-29 20:51:04 +0530

    completes 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...theArgs) {
+  for (let arg of theArgs) {
+    for (let i = 0; i < array.length; i++) {
+      if (arg === array[i]) {
+        array.splice(i, 1);
+      }
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
