822fa4a6a11a2539b257ec0d65b23904d78f8c9f
MBanerjea <mayukh.flip22@gmail.com>
2023-01-30 19:34:13 +0530

    completes 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,22 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  let sum = 0;
+  // console.log(typeof num1);
+  if (typeof num1 != "number" || num1 < 0 || typeof num2 != "number" || num2 < 0) {
+    return "ERROR";
+  } else {
+    if (num1 < num2) {
+      for (let i = num1; i <= num2; i++) {
+        sum += i;
+      }
+    } else if (num2 < num1) {
+      for (let i = num2; i <= num1; i++) {
+        sum += i;
+      }
+    } else {
+      sum = num1;
+    }
+    return sum;
+  }
 
 };
 
